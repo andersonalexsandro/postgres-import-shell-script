9aae2c6c7f653a6733f6c6687ae52d62341d4103
andersonalexsandro <anderson.silva.04@outlook.com>
2024-09-26 18:01:20 -0300

    refactor: agency_operations approves staging

--- sql/04-user_202405221529.sql
+++ sql/04-user_202405221529.sql
@@ -1,9 +1,9 @@
 INSERT INTO public."user" 
-("name",cpf,cellphone,identifier,email,agency_id,"password","role",id,created_at,updated_at)
+("name",cpf,cellphone,siape,email,agency_id,"password","role",id,created_at,updated_at)
 VALUES
 	 ('Importação do Sistema','33425342223','83999999999','1234567','system@admin.com',NULL,'$pbkdf2-sha256$29000$5xwjxPg/Z8xZi5GSklLKOQ$ngeZK3tqws5iUxF/nyclSI6vmGS2iQNEj2vTloPfm2o','adm','a2495bfa-c6ff-4f82-b2f1-5f06ca87df9b','2024-05-21 19:06:23.156266','2024-05-21 19:06:23.156269');
 
 INSERT INTO public."user" 
-("name", cpf, cellphone, identifier, email, agency_id, "password", "role", id, created_at, updated_at)
+("name", cpf, cellphone, siape, email, agency_id, "password", "role", id, created_at, updated_at)
 VALUES
 ('Anderson Alexsandro Soares da Silva', '12266317466', '83999999998', '1234568', 'anderson.silva.04@outlook.com', '00d43d38-bb3d-4ee3-a2f8-6eb0c44e2593', '$pbkdf2-sha256$29000$5xwjxPg/Z8xZi5GSklLKOQ$ngeZK3tqws5iUxF/nyclSI6vmGS2iQNEj2vTloPfm2o', 'adm', 'ea46d072-e448-4811-9d10-15cbf1ded10f', '2024-05-21 19:06:24.156266', '2024-05-21 19:06:24.156269');
